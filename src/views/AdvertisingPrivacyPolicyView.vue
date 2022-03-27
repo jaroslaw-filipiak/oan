0915ce5a2e0204640998de5fdeefa2761e314505
Jarosław Filipiak <info@j-filipiak.pl>
2022-03-27 08:12:37 +0200

    Simple content pages content

--- src/views/AdvertisingPrivacyPolicyView.vue
+++ src/views/AdvertisingPrivacyPolicyView.vue
@@ -1,3 +1,37 @@
 <template>
-    <h1>Privacy policy advertising</h1>
+   <section class="content-page">
+     <div class="container-fluid">
+       <div class="container">
+
+         <div class="row">
+           <div class="col"><h1>Polityka Prywatności Usług Reklamowych</h1></div>
+         </div>
+
+         <div class="row">
+           <div class="col">
+
+             <h3> 1. Informacje ogólne</h3>
+
+             <ul>
+               <li>W niniejszej Polityce prywatności określenia “my”, “nasza” odnosi się do spółki pod firmą Online Advertising Network Sp. z o.o. z siedzibą w Warszawie (05-075), przy ul. Żeromskiego 7, wpisaną do rejestru przedsiębiorców prowadzonego przez Sąd Rejonowy dla m.st. w Warszawy w Warszawie, XIII Wydział Gospodarczy Krajowego Rejestru Sądowego pod numerem 0000524099, NIP: 9512384089, REGON: 147425174.
+               </li>
+
+               <li>
+                 Niniejsza Polityka ma zastosowanie w odniesieniu do świadczonych przez nas usług reklamowych w Internecie. Usługi reklamowe obejmują m.in. usługi reklamowe oparte o zainteresowania użytkowników oraz o usługi retargetingu.
+               </li>
+
+               <li>Skontaktować się można z nami tradycyjnie, pisząc na adres naszej siedziby, mailowo (team@oan.pl) i telefonicznie (225353050).
+               </li>
+
+               <li>W każdym przypadku dotyczącym przetwarzania danych można się skontaktować z osobą odpowiedzialną za ochronę danych pod następującym adresem: privacy@oan.pl
+               </li>
+             </ul>
+
+           </div>
+
+         </div>
+
+       </div>
+     </div>
+   </section>
 </template>
